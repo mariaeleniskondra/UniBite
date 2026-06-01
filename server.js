1d7d4ae4e952e7162aa0ba042ab28306c869f91e
MARIA ELENI SKONDRA <up1108330@ac.upatras.gr>
2026-06-01 20:55:06 +0300

    Refactor: db connection moved to models/db.js

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
-require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
+require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2');
-
 const app = express();
+
+require('./models/db'); 
 
 app.use(express.json());
 app.use(express.static('Public'));
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Σφάλμα σύνδεσης:', err.message);
-    return;
-  }
-  console.log('Συνδέθηκε στη MySQL!');
-});
+// Routes (θα τα προσθέτουμε σταδιακά)
+// app.use('/api/auth', require('./routes/auth'));
 
 app.listen(process.env.PORT, () => {
   console.log(`Server τρέχει στο http://localhost:${process.env.PORT}`);
 });
-
-module.exports = db;
